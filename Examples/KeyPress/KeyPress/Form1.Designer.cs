770247e4cf727fda460ff9ce65c038026e6c4c49
rosstimo <rosstimo@isu.edu>
2026-02-11 13:59:06 -0800

    key press visualizer

---
+++ Examples/KeyPress/KeyPress/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace KeyPress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKeyDisplay = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            
+            // lblKeyDisplay
+            this.lblKeyDisplay.AutoSize = false;
+            this.lblKeyDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblKeyDisplay.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.lblKeyDisplay.Location = new System.Drawing.Point(50, 50);
+            this.lblKeyDisplay.Name = "lblKeyDisplay";
+            this.lblKeyDisplay.Size = new System.Drawing.Size(700, 350);
+            this.lblKeyDisplay.TabIndex = 0;
+            this.lblKeyDisplay.Text = "Press any key...";
+            this.lblKeyDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblKeyDisplay);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Key Press Display";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKeyDisplay;
+    }
+}
+
---
+++ Examples/KeyPress/KeyPress/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyPress
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            UpdateKeyDisplay(e, "DOWN");
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            lblKeyDisplay.Text = "Press any key...";
+        }
+
+        private void UpdateKeyDisplay(KeyEventArgs e, string state)
+        {
+            StringBuilder keyInfo = new StringBuilder();
+            
+            keyInfo.AppendLine($"Key: {e.KeyCode}");
+            keyInfo.AppendLine($"KeyData: {e.KeyData}");
+            keyInfo.AppendLine($"KeyValue: {e.KeyValue}");
+            keyInfo.AppendLine();
+            
+            keyInfo.AppendLine("Modifiers:");
+            keyInfo.AppendLine($"  Ctrl: {e.Control}");
+            keyInfo.AppendLine($"  Alt: {e.Alt}");
+            keyInfo.AppendLine($"  Shift: {e.Shift}");
+            
+            lblKeyDisplay.Text = keyInfo.ToString();
+        }
+    }
+}
