40a04746cacc6eb5b06ed1342d3f5a6d52a9b320
rosstimo <rosstimo@isu.edu>
2026-02-17 09:42:35 -0800

    add write, append,, read examples

--- Examples/FileIOExample/FileIOExample/Program.cs
+++ Examples/FileIOExample/FileIOExample/Program.cs
@@ -4,11 +4,44 @@
     {
         static void Main(string[] args)
         {
+            string filePath = "..\\..\\..\\TestFile.txt";
 
-
-
+            //WriteToFile(filePath);
+            //AppendToFile(filePath);
+            ReadFromFile(filePath);
+            
             //pause
             Console.Read();
         }
+
+        static void WriteToFile(string path)
+        {
+            using (StreamWriter currentFile = File.CreateText(path))
+            {
+                currentFile.WriteLine("Wake up Neo...");
+            }
+        }
+
+        static void AppendToFile(string path)
+        {
+            using (StreamWriter currentFile = File.AppendText(path))
+            {
+                currentFile.WriteLine("Follow the white rabbit...");
+            }
+        }
+
+        static void ReadFromFile(string path)
+        {
+            using (StreamReader currentFile = new StreamReader(path))
+            {
+                Console.WriteLine(currentFile.ReadLine());
+                Console.WriteLine(currentFile.ReadLine());
+                Console.WriteLine(currentFile.ReadLine());
+                Console.WriteLine(currentFile.ReadLine());//pay read past end of file
+                Console.WriteLine(currentFile.ReadLine());
+                Console.WriteLine(currentFile.ReadLine());
+            }
+        }
+
     }
 }
