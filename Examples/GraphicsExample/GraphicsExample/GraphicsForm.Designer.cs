ab0fec765e32af9639e74a3fa3d9cd85fa395fb6
rosstimo <rosstimo@isu.edu>
2026-02-19 09:31:15 -0800

    review draw line. add draw dart

--- Examples/GraphicsExample/GraphicsExample/GraphicsForm.Designer.cs
+++ Examples/GraphicsExample/GraphicsExample/GraphicsForm.Designer.cs
@@ -28,21 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.DisplayPictureBox = new System.Windows.Forms.PictureBox();
+            this.ButtonGroupBox = new System.Windows.Forms.GroupBox();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.DrawButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DisplayPictureBox)).BeginInit();
+            this.ButtonGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // DisplayPictureBox
+            // 
+            this.DisplayPictureBox.Location = new System.Drawing.Point(23, 49);
+            this.DisplayPictureBox.Name = "DisplayPictureBox";
+            this.DisplayPictureBox.Size = new System.Drawing.Size(741, 262);
+            this.DisplayPictureBox.TabIndex = 0;
+            this.DisplayPictureBox.TabStop = false;
+            // 
+            // ButtonGroupBox
+            // 
+            this.ButtonGroupBox.Controls.Add(this.DrawButton);
+            this.ButtonGroupBox.Controls.Add(this.ExitButton);
+            this.ButtonGroupBox.Location = new System.Drawing.Point(23, 317);
+            this.ButtonGroupBox.Name = "ButtonGroupBox";
+            this.ButtonGroupBox.Size = new System.Drawing.Size(741, 112);
+            this.ButtonGroupBox.TabIndex = 1;
+            this.ButtonGroupBox.TabStop = false;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(574, 25);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(161, 65);
+            this.ExitButton.TabIndex = 0;
+            this.ExitButton.Text = "E&xit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // DrawButton
+            // 
+            this.DrawButton.Location = new System.Drawing.Point(407, 25);
+            this.DrawButton.Name = "DrawButton";
+            this.DrawButton.Size = new System.Drawing.Size(161, 65);
+            this.DrawButton.TabIndex = 1;
+            this.DrawButton.Text = "&Draw";
+            this.DrawButton.UseVisualStyleBackColor = true;
+            this.DrawButton.Click += new System.EventHandler(this.DrawButton_Click);
+            // 
+            // GraphicsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.Controls.Add(this.ButtonGroupBox);
+            this.Controls.Add(this.DisplayPictureBox);
+            this.Name = "GraphicsForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DisplayPictureBox)).EndInit();
+            this.ButtonGroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox DisplayPictureBox;
+        private System.Windows.Forms.GroupBox ButtonGroupBox;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button DrawButton;
     }
 }
 
--- Examples/GraphicsExample/GraphicsExample/GraphicsForm.cs
+++ Examples/GraphicsExample/GraphicsExample/GraphicsForm.cs
@@ -17,9 +17,48 @@ namespace GraphicsExample
             InitializeComponent();
         }
 
+        void DrawLine()
+        {
+            Graphics g = DisplayPictureBox.CreateGraphics();
+            Pen thePen = new Pen(Color.Red, 1);
+            
+            g.DrawLine(thePen, 0, 0, 100, 100);
+
+            g.Dispose();
+            thePen.Dispose();
+        }
+
+        void DrawDart(int x, int y)
+        {
+            Graphics g = DisplayPictureBox.CreateGraphics();
+            Pen thePen = new Pen(Color.Blue, 1);
+            int size = 30;
+
+            g.DrawEllipse(thePen, x - size/2, y - size/2, size, size);
+
+            g.DrawLine(thePen, x - 5, y, x + 5, y);
+            g.DrawLine(thePen, x, y - 5, x, y + 5);
+
+            g.Dispose();
+            thePen.Dispose();
+        }
+
+
+        // Event handlers below _______________________________________________
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close(); 
+        }
+
+        private void DrawButton_Click(object sender, EventArgs e)
+        {
+            DrawLine();
+            DrawDart(100,100);
         }
     }
 }
