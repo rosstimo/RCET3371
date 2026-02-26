20bc91b5b5490d5d3d39574d257920063ed04d71
rosstimo <rosstimo@isu.edu>
2026-02-26 09:49:05 -0800

    test cards in a List

--- Examples/OOPExample/OOPExample/Deck.cs
+++ Examples/OOPExample/OOPExample/Deck.cs
@@ -4,7 +4,39 @@ using System.Text;
 
 namespace OOPExample
 {
+    /*
+    TODO:
+    [x] create 52 cards 
+    [ ] keep cards in a collection (list, stack, que) 
+    [ ] deal a card and remove it from the collection 
+    [ ] provide number of cards remaining
+    [ ] shuffle the deck (refresh/recreate cards in a full deck. maybe randomize)
+    */
     internal class Deck
     {
+        private List<string> dinosaurs = new List<string>();
+        Queue<string> numbers = new Queue<string>();
+        Stack<string> letter = new Stack<string>();
+        private List<Card> _deck = new List<Card>();
+
+        
+        public Deck() 
+        {
+            for (int suitIndex = 0; suitIndex < 3; suitIndex++)
+            {
+                for (int rankIndex = 0; rankIndex < 12; rankIndex++)
+                {
+                    Card currentCard = new Card(rankIndex, suitIndex);
+                    this._deck.Add(currentCard);
+                    //Console.WriteLine($"{currentCard.Rank()} of {currentCard.Suit()}");
+                }
+            }
+            //this._deck.Shuffle();
+
+            foreach  (Card theCard in this._deck.Shuffle())
+            {
+                Console.WriteLine($"{theCard.Rank()} of {theCard.Suit()}");
+            }
+        }
     }
 }
--- Examples/OOPExample/OOPExample/Program.cs
+++ Examples/OOPExample/OOPExample/Program.cs
@@ -5,14 +5,17 @@
         static void Main(string[] args)
         {
 
-            Card myCard = new Card(13,13);
-            Card AS = new Card(1,0);
-            Card AD = new Card(1,1);
-            //myCard.RankIndex = 1; //read only
+            //Card myCard = new Card(13,13);
+            //Card AS = new Card(1,0);
+            //Card AD = new Card(1,1);
+            ////myCard.RankIndex = 1; //read only
             
-            Console.WriteLine($"{myCard.Rank()} of {myCard.Suit()}");
-            Console.WriteLine($"{AS.Rank()} of {AS.Suit()}");
-            Console.WriteLine($"{AD.Rank()} of {AD.Suit()}");
+            //Console.WriteLine($"{myCard.Rank()} of {myCard.Suit()}");
+            //Console.WriteLine($"{AS.Rank()} of {AS.Suit()}");
+            //Console.WriteLine($"{AD.Rank()} of {AD.Suit()}");
+
+            Deck newDeck = new Deck();
+
 
             //pause
             Console.Read();
