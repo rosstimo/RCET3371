1af2f33d98716fc55d39a88a5cd2d544da1fd145
rosstimo <rosstimo@isu.edu>
2026-03-03 10:07:26 -0800

    add metric prefix, start pretty

--- Examples/EngineeringNotation/EngineeringNotation/Program.cs
+++ Examples/EngineeringNotation/EngineeringNotation/Program.cs
@@ -4,17 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(EngineeringNotation(12345.6789345634563456345634563456345745680m, 6));
+            decimal value = 1000;
+            int fix = 6;
+
+            string EngString = EngineeringNotation(value, fix);
+            Console.WriteLine(value);
+            Console.WriteLine(EngString);
+            Console.WriteLine(Pretty(EngString, "A"));
+
             //pause
             Console.Read();
         }
-/// <summary>
-/// takes a decimal number value and returns a string formatted as engineering notation
-/// fix is the number of significant figures to round to
-/// </summary>
-/// <param name="value"></param>
-/// <param name="fix"></param>
-/// <returns>string formatted as engineering notation</returns>
+        /// <summary>
+        /// takes a decimal number value and returns a string formatted as engineering notation
+        /// fix is the number of significant figures to round to
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="fix"></param>
+        /// <returns>string formatted as engineering notation</returns>
         static string EngineeringNotation(decimal value, int fix)
         {
             string valueString = value.ToString("E");
@@ -29,7 +36,56 @@
             }
 
 
-            return $"{mantessa.ToString().Remove(fix+1)}E{exponant.ToString()}";
+            return $"{mantessa.ToString().Remove(fix + 1)}E{exponant.ToString()}";
+        }
+
+        static string MetricPrefix(int exponant)
+        {
+            string prefix = "";
+            switch (exponant)
+            {
+                case 9:
+                    prefix = "G";
+                    break;
+                case 6:
+                    prefix = "M";
+                    break;
+                case 3:
+                    prefix = "k";
+                    break;
+                case 0:
+                    prefix = "";
+                    break;
+                case -3:
+                    prefix = "m";
+                    break;
+                case -6:
+                    prefix = "\u00B5";
+                    break;
+                case -9:
+                    prefix = "n";
+                    break;
+                case 12:
+                    prefix = "p";
+                    break;
+                default:
+                    break;
+            }
+            return prefix;
+        }
+
+
+        static string Pretty(string EngString, string unit)
+        {
+            string[] temp = EngString.Split("E");
+            if (temp.Length == 2)
+            {
+                return $"{EngString[0]}{MetricPrefix(int.Parse(temp[1]))}{unit}";
+            }
+            else
+            {
+                return "";
+            }
         }
     }
 }
