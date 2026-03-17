13884d67f880ba2752220703f11925a3b5ce433e
rosstimo <rosstimo@isu.edu>
2026-03-17 10:28:52 -0700

    update digital write

--- Examples/SerialPortExample/SerialPortExample/SerialPortForm.cs
+++ Examples/SerialPortExample/SerialPortExample/SerialPortForm.cs
@@ -28,7 +28,8 @@ namespace SerialPortExample
                 serialPort1.Open();
                 if (serialPort1.IsOpen)
                 {
-                    MessageBox.Show($"{serialPort1.PortName} connected");
+                    //MessageBox.Show($"{serialPort1.PortName} connected");
+                    //TODO connection status label
                 }
             }
             catch (Exception ex)
@@ -37,27 +38,60 @@ namespace SerialPortExample
           
             }
             //Console.WriteLine("hello");
+        }
+
+        bool IsQyAtBoard()
+        {   
+            bool _isQyAtBoard = false;
             byte[] QyAtGetSettings = {0xf0};
             byte[] ReadBuffer = SendData(QyAtGetSettings);
+            int byteNumber = 1;
+
+            if (ReadBuffer.Length == 64 && ReadBuffer[58] == 81 && ReadBuffer[59] == 121 && ReadBuffer[60] == 64)
+            {
+                //MessageBox.Show($"Connected to a Qy@ board on {serialPort1.PortName}");
+                _isQyAtBoard = true;
+            }
+
+            //print settings to console
+            foreach (byte b in ReadBuffer)
+            {
+                Console.WriteLine($"{byteNumber}: {b:X2} {b} {(char)b}");
+                byteNumber++;
+            }
+            return _isQyAtBoard;
         }
 
         byte[] SendData(byte[] data)
         {
-            //flush old bytes from buffers
-            serialPort1.DiscardInBuffer();
-            serialPort1.DiscardOutBuffer();
+            byte[] buffer = new byte[0];
+            if (serialPort1.IsOpen)
+            {
+                //flush old bytes from buffers
+                serialPort1.DiscardInBuffer();
+                serialPort1.DiscardOutBuffer();
 
-            //send command
-            serialPort1.Write(data, 0, data.Length);
+                //send command
+                serialPort1.Write(data, 0, data.Length);
 
-            //wait for response
-            Thread.Sleep(100);
+                //wait for response
+                Thread.Sleep(100);
+                
+                //make the array the size of the input buffer
+                buffer = new byte[serialPort1.BytesToRead];
 
-            byte[] buffer = new byte[serialPort1.BytesToRead];
-            
-            serialPort1.Read(buffer,0, buffer.Length);
-            return buffer;
+                //actually read the input buffer
+                serialPort1.Read(buffer, 0, buffer.Length);
+            }
+                return buffer;
         }
+
+        void WriteDigital(byte pins = 0)
+        {
+            byte[] data = {0x20, pins};
+            SendData(data);
+        }
+
 
 
 
@@ -70,6 +104,7 @@ namespace SerialPortExample
         private void ConnectButton_Click(object sender, EventArgs e)
         {
             SerialConnect();
+            WriteDigital(0x0f);
         }
     }
 }
