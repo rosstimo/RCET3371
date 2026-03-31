3a46eba434cfb857b4664311e3504e75580592f8
rosstimo <rosstimo@isu.edu>
2026-03-31 09:07:51 -0600

    rename

--- Examples/DataLogging/DataLogging/DataLoggingForm.cs
+++ Examples/DataLogging/DataLogging/DataLoggingForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace DataLogging
 {
-    public partial class Form1 : Form
+    public partial class DataLoggingForm : Form
     {
-        public Form1()
+        public DataLoggingForm()
         {
             InitializeComponent();
         }
--- Examples/DataLogging/DataLogging/Program.cs
+++ Examples/DataLogging/DataLogging/Program.cs
@@ -16,7 +16,7 @@ namespace DataLogging
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DataLoggingForm());
         }
     }
 }
