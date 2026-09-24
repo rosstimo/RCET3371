c73f3957563c8120df4fc35aacdbb4dc13c6ebcb
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2026-09-24 18:53:53 +0000

    Add S27 student on-ramp setup examples

---
+++ Examples/OnRamp/CSharp/Program.cs
@@ -0,0 +1,11 @@
+int first = 2;
+int second = 3;
+int answer = Add(first, second);
+
+Console.WriteLine("Hello, RCET 3371!");
+Console.WriteLine($"{first} + {second} = {answer}");
+
+static int Add(int first, int second)
+{
+    return first + second;
+}
