212a29715d409b17404e765ab82399ca8e8e4337
rosstimo <rosstimo@isu.edu>
2026-09-24 09:24:27 -0600

    adjust start position

--- Examples/F26Review/F26Review/ReviewForm.Designer.cs
+++ Examples/F26Review/F26Review/ReviewForm.Designer.cs
@@ -60,7 +60,8 @@
             Controls.Add(DrawPictureBox);
             Controls.Add(ExitButton);
             Name = "ReviewForm";
-            Text = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Review";
             ((System.ComponentModel.ISupportInitialize)DrawPictureBox).EndInit();
             ResumeLayout(false);
         }
